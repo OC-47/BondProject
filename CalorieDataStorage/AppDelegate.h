104e1b4b79d0171a6948476800b8e6074b538f95
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-10-29 16:36:53 +0900

    Initial Commit

---
+++ CalorieDataStorage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalorieDataStorage
+//
+//  Created by Miwa Oshiro on 2013/10/29.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
