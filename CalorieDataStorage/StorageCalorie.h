13f522942fd8a6427c4a7f7614c0dc5bb7e8be54
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-11-01 16:00:42 +0900

    Storage Calorie db, file name change.

---
+++ CalorieDataStorage/StorageCalorie.h
@@ -0,0 +1,21 @@
+//
+//  StorageCalorie.h
+//  CalorieDataStorage
+//
+//  Created by Miwa Oshiro on 2013/10/30.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StorageCalorie : UIViewController <UITextFieldDelegate>
+
+
+@property (strong, nonatomic) IBOutlet UITextField *calorieTitle;
+@property (strong, nonatomic) IBOutlet UITextField *calorieCal;
+@property (strong, nonatomic) IBOutlet UITextField *calorieNum;
+
+- (IBAction)calorieSave:(id)sender;
+
+
+@end
---
+++ CalorieDataStorage/StorageCalorie.m
@@ -0,0 +1,103 @@
+//
+//  StorageCalorie.m
+//  CalorieDataStorage
+//
+//  Created by Miwa Oshiro on 2013/10/30.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import "StorageCalorie.h"
+#import "FMDatabase.h"
+
+@interface StorageCalorie ()
+
+@end
+
+@implementation StorageCalorie;
+
+@synthesize calorieTitle;
+@synthesize calorieCal;
+@synthesize calorieNum;
+
+sqlite3* db;
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+
+    //DBファイルのパス
+    NSArray *paths = NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES );
+    NSString *dir   = [paths objectAtIndex:0];
+    
+    //DBファイルがあるかどうか確認
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+
+
+
+    if (![fileManager fileExistsAtPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]])
+    {
+        //なければ新規作成
+        FMDatabase *db= [FMDatabase databaseWithPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]];
+        
+        NSString *sql = @"CREATE TABLE calorie_data (id INTEGER PRIMARY KEY AUTOINCREMENT,calorie_title TEXT,calorie_cal INTEGER,calorie_num INTEGER,date TEXT);";
+        
+        [db open]; //DB開く
+        [db executeUpdate:sql]; //SQL実行
+        [db close];
+    }
+
+
+}
+
+
+- (IBAction)calorieSave:(id)sender {
+
+
+    NSString *calorie_title = self.calorieTitle.text;
+    
+    float calorie_cal_f = [self.calorieCal.text floatValue];
+    NSNumber *calorie_cal = [NSNumber numberWithFloat:calorie_cal_f];
+    
+    float calorie_num_f = [self.calorieCal.text floatValue];
+    NSNumber *calorie_num = [NSNumber numberWithFloat:calorie_num_f];
+    
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES );
+    NSString *dir   = [paths objectAtIndex:0];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+
+    
+    
+    if ([fileManager fileExistsAtPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]])
+    {
+        FMDatabase *db= [FMDatabase databaseWithPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]];
+        
+        [db open]; //DB開く
+        
+        NSDateFormatter *df = [[NSDateFormatter alloc] init];
+        df.dateFormat  = @"yyyy-MM-dd HH:mm:ss";
+        NSString *strDate = [df stringFromDate:[NSDate date]];
+        
+        [db executeUpdate:@"insert into calorie_data (calorie_title,calorie_cal,calorie_num,date) values (?,?,?,?);",calorie_title,calorie_cal,calorie_num,strDate];
+        
+        NSLog(@"Error %@ - %d", [db lastErrorMessage], [db lastErrorCode]);
+        [db close];
+    }
+
+    
+}
+@end
