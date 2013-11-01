15e662269737508fbf228c0412b80b81427e5ac7
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-11-01 15:08:21 +0900

    add calorie_data db.

--- CalorieDataStorage/StorageCarorie.m
+++ CalorieDataStorage/StorageCarorie.m
@@ -7,12 +7,18 @@
 //
 
 #import "StorageCarorie.h"
+#import "FMDatabase.h"
 
 @interface StorageCarorie ()
 
 @end
 
-@implementation StorageCarorie
+@implementation StorageCarorie;
+@synthesize calorieTitle;
+@synthesize calorieCal;
+@synthesize calorieNum;
+
+sqlite3* _db;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -21,15 +27,33 @@
         // Custom initialization
     }
     return self;
+
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //DBファイルのパス
+    NSArray *paths = NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES );
+    NSString *dir   = [paths objectAtIndex:0];
+    
+    //DBファイルがあるかどうか確認
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if (![fileManager fileExistsAtPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]])
+    {
+        //なければ新規作成
+        FMDatabase *db= [FMDatabase databaseWithPath:[dir stringByAppendingPathComponent:@"calorie_data.db"]];
+        
+        NSString *sql = @"CREATE TABLE calorie_data (id INTEGER PRIMARY KEY AUTOINCREMENT,calorie_title TEXT,calorie_cal INTEGER,calorie_num INTEGER);";
+        
+        [db open]; //DB開く
+        [db executeUpdate:sql]; //SQL実行
+        [db close];
+    }
 
 }
-
-
 
 
 @end
