3c5c2760bdf4b17c8811d5434be78b133c8c3d78
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-11-01 14:52:34 +0900

    page move button -> calorie.

---
+++ CalorieDataStorage/StorageCarorie.h
@@ -0,0 +1,13 @@
+//
+//  StorageCarorie.h
+//  CalorieDataStorage
+//
+//  Created by Miwa Oshiro on 2013/10/30.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StorageCarorie : UIViewController
+
+@end
---
+++ CalorieDataStorage/StorageCarorie.m
@@ -0,0 +1,35 @@
+//
+//  StorageCarorie.m
+//  CalorieDataStorage
+//
+//  Created by Miwa Oshiro on 2013/10/30.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import "StorageCarorie.h"
+
+@interface StorageCarorie ()
+
+@end
+
+@implementation StorageCarorie
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+
+
+@end
